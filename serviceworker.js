023737fa36f11965abb5bc49ed693a41ac387150
idotta <dotta.iuri@gmail.com>
2019-11-14 18:34:32 -0300

    Update serviceworker and icons

--- serviceworker.js
+++ serviceworker.js
@@ -1,56 +1,29 @@
 console.log("This is service worker talking!");
-var cacheName = 'blazor-test';
+var cacheName = 'cpp-pwa';
 var filesToCache = [
     './',
     //Html and css files
     './index.html',
-    './css/site.css',
-    './css/bootstrap/bootstrap.min.css',
-    './css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    './css/open-iconic/font/fonts/open-iconic.woff',
-    //Blazor framework
-    './_framework/blazor.webassembly.js',
-    './_framework/blazor.boot.json',
-    './_framework/blazor.server.js',
     //Our additional files
     './manifest.json',
     './serviceworker.js',
     './icons/icon-192x192.png',
     './icons/icon-512x512.png',
-    //The web assembly/.net dll's
-    './_framework/wasm/mono.js',
-    './_framework/wasm/mono.wasm',
-    //'./_framework/_bin/Microsoft.AspNetCore.Blazor.Browser.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Authorization.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Components.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Components.Forms.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Components.Web.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Metadata.dll',
-    '/_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll',
-    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-    '/_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll',
-    '/_framework/_bin/Microsoft.Extensions.Options.dll',
-    '/_framework/_bin/Microsoft.Extensions.Primitives.dll',
-    '/_framework/_bin/Microsoft.JSInterop.dll',
-    '/_framework/_bin/Mono.Security.dll',
-    '/_framework/_bin/Mono.WebAssembly.Interop.dll',
-    '/_framework/_bin/mscorlib.dll',
-    '/_framework/_bin/System.Buffers.dll',
-    '/_framework/_bin/System.ComponentModel.Annotations.dll',
-    '/_framework/_bin/System.Core.dll',
-    '/_framework/_bin/System.dll',
-    '/_framework/_bin/System.Memory.dll',
-    '/_framework/_bin/System.Net.Http.dll',
-    '/_framework/_bin/System.Numerics.Vectors.dll',
-    '/_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll',
-    '/_framework/_bin/System.Text.Encodings.Web.dll',
-    '/_framework/_bin/System.Text.Json.dll',
-    '/_framework/_bin/System.Threading.Tasks.Extensions.dll',
-    //The compiled project .dll's
-    './_framework/_bin/BlazorTest.dll'
+    // Compiled files
+    "./app.asm.69bcd8d2.js",
+    "./app.b7bdb114.map",
+    "./src.2d567fa5.map",
+    "./app.asm.69bcd8d2.map",
+    "./serviceworker.map",
+    "./src.996355c5.css",
+    "./app.b7bdb114.js",
+    "./src.2d567fa5.js",
+    // asmjs files
+    "./asmjs/app.asm.js",
+    "./asmjs/app.asm.js.mem",
+    // wasm files
+    "./wasm/app.js",
+    "./wasm/app.wasm"
 ];
 
 self.addEventListener('install', function (e) {
